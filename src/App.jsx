ba94203bce1f0dd8e7f36382cdc1ef41634e1970
1uciuszzz <nick_zzhao@outlook.com>
2022-06-15 10:58:11 +0800

    add tailwindcss and its plugins

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-import Logo from "./assets/undraw_react_re_g3ui.svg";
-import "./App.css";
-
+import "./dist.css";
 export default function App() {
   return (
-    <div className="logoContainer">
-      <Logo />
+    <div className="h-screen grid place-items-center">
+      <article className="prose">
+        <h1>typography example</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quidem
+          repellat, beatae delectus nam deleniti mollitia sed qui. Obcaecati
+          dolorem ipsam facilis laudantium et! Modi corporis distinctio at dolor
+          consequatur!
+        </p>
+        <blockquote className="line-clamp-2">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis iusto,
+          fugiat esse ipsam sapiente omnis, reiciendis fuga quisquam tempora
+          architecto, quaerat quibusdam earum hic facilis! Magni laudantium
+          maxime ad sit.
+        </blockquote>
+      </article>
     </div>
   );
 }
